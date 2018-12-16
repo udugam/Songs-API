3ecf371f9871c66da4dd6f53314cf1e7acd10773
udugam <ryan.udugam@gmail.com>
2018-12-15 19:28:35 -0500

    added song name and artist to post body

--- routes.js
+++ routes.js
@@ -1,5 +1,19 @@
+require('dotenv').config()
+const Spotify = require('node-spotify-api')
+
+const spotify = new Spotify({
+    id: process.env.SPOTIFY_CLIENT_ID,
+    secret: process.env.SPOTIFY_CLIENT_SECRET
+});
+
 module.exports = function(app) {
     app.post('/getSpotifyID', function(req,res) {
-        res.json("this is the Spotify ID")
+        spotify.search({ type: 'track', query: req.body.songName, limit: 1 })
+        .then(function(response) {
+            res.json(response.tracks.items[0].id)
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
     })
 }
