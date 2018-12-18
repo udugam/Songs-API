ce3d3f52394278665e67429d664a1d2dbf5e4c7f
udugam <ryan.udugam@gmail.com>
2018-12-18 19:53:49 +0000

    get apple music ID is completed

--- routes.js
+++ routes.js
@@ -9,6 +9,7 @@ const spotify = new Spotify({
 
 module.exports = function(app, token) {
     app.post('/getSpotifyID', function(req,res) {
+        console.log(req.body)
         spotify.search({ type: 'track', query: req.body.songName, limit: 1 })
         .then(function(response) {
             res.json(response.tracks.items[0].id)
@@ -20,17 +21,17 @@ module.exports = function(app, token) {
     app.post('/getAppleMusicID', function(req,res) {
         (async () => {
             try {
-                const response = await got('catalog/us/search?term=james+brown&limit=2&types=artists,albums', 
-                {baseUrl: 'https://api.music.apple.com/v1/',
-                 headers: {
-                     'Authorization': `Bearer ${token}`
-                 }
+                const response = await got(`catalog/us/search?term=${req.body.artistName.trim().replace(" ","+")}+${req.body.songName.trim().replace(" ","+")}&limit=1&types=songs`, 
+                {
+                    baseUrl: 'https://api.music.apple.com/v1/',
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    },
+                    json: 'true'
                 })
-                res.json(response.body);
-                //=> '<!doctype html> ...'
+                res.json(response.body.results.songs.data[0].id);
             } catch (error) {
                 res.json(error.response.body);
-                //=> 'Internal server error ...'
             }
         })();
     })
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 var keyID = process.env.APPLE_MUSIC_KEY_ID
 var teamID = process.env.APPLE_TEAM_ID
-var secret = fs.readFileSync("AuthKey_6F2LBRA7TK.p8").toString();
+var secret = fs.readFileSync("AuthKey.p8").toString();
 var token = jwt.generate(keyID, teamID, secret);
 
 //Import routes
